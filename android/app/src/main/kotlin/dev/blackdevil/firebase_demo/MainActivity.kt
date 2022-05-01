766fa85247a3a6e034e81f78387a497bffc456a8
Zinzuvadiya Meet <zinzuvadiyameet98@gmail.com>
2022-05-01 11:33:03 +0530

    Firebase_init and get_data function added

---
+++ android/app/src/main/kotlin/dev/blackdevil/firebase_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.blackdevil.firebase_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_demo/get_data.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CheckFirebaseConnection(),
+    );
+  }
+}
+
+class CheckFirebaseConnection extends StatefulWidget {
+  const CheckFirebaseConnection({Key? key}) : super(key: key);
+
+  @override
+  State<CheckFirebaseConnection> createState() =>
+      _CheckFirebaseConnectionState();
+}
+
+class _CheckFirebaseConnectionState extends State<CheckFirebaseConnection> {
+  Future<FirebaseApp> firebaseInit = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: firebaseInit,
+      builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const GetDataFromFirestore();
+        } else {
+          return textWidget(snapshot.error.toString());
+        }
+      },
+    );
+  }
+}
+
+textWidget(text) {
+  return Scaffold(
+    body: Center(
+      child: Text(text),
+    ),
+  );
+}
